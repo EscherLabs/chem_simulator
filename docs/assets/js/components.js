2ff0863a0767c73d135d14d696c9374c8b14a6db
Adam Smallcomb <atsmallcomb@gmail.com>
2020-07-15 14:51:46 -0400

    gc temp variations

--- docs/assets/js/components.js
+++ docs/assets/js/components.js
@@ -3,6 +3,42 @@ instrument_components = [
       legend:'Gas Chromatograph',
       sections:'tab',
       name:'GC',
+      methods:{
+        onAdd:function(e){
+          if(e.field.index > 0){
+            var temp = e.form.find({map:'column_oven.temperature_program.'+(e.field.index-1)})
+            ;
+            e.field.find('temperature').update({min:temp.get('temperature'),value:temp.get('temperature'),max:250})
+            // e.field.fields[1].update({value:(100 - (temp.get('solvent_b')+1))})
+            // e.field.fields[2].update({min:(temp.get('solvent_b')+1),value:(temp.get('solvent_b')+1)})
+          }else{
+            e.field.find('temperature').update({min:100,max:150})
+
+          }
+
+        },    
+        done:function(e){
+          debugger;
+          // var temp =  e.form.filter({name:'temperature_program'})
+          // _.reduce(e.form.filter({name:'temperature_program'}),function(time,item){
+          //   debugger;
+          //   item.find('time').update({value:time})
+          //   time += parseInt(item.get('hold_time'))
+          //   return time;
+          // },0)
+
+        }
+      },
+      "events": [
+        {
+          "event": "modal",
+          "handler": "onAdd"
+        },
+        {
+          "event": "done",
+          "handler": "done"
+        }
+      ],
       fields:[
         {legend: 'Detector', type: 'fieldset',fields:[
           {name:"Temperature (°C)",type:"number",value:100,min:100,max:200,step:5,validate:[{type:'numeric'}]},
@@ -17,8 +53,9 @@ instrument_components = [
             {name:"Temperature (°C)",type:"number",value:100,min:100,max:200,step:5},
             {name:"Run time (min)",type:"number",value:0,min:0,max:10,step:.5},
           ]},
-          {legend: 'Temperature Program',array:{min:1}, type: 'table',fields:[
+          {legend: 'Temperature Program',name:"temperature_program",array:{min:1}, type: 'table',fields:[
             {label:"Time (min)",edit:false,name:"time",type:"number",template:"{{value}}{{^value}}(origin){{/value}}",value:function(e){
+              debugger;
                 if(e.initial.parent.index == e.field.parent.index && e.field.name == "time"){
                     var old = e.field.parent.parent.find({name:"temperature_program",index:e.initial.parent.index-1});
                     if(old){
@@ -30,8 +67,8 @@ instrument_components = [
                     return e.initial.value   
                 }
             }},
-            {label:"Temperature (°C)",type:"number",value:100,min:100,max:250,step:5},
-            {label:"Ramp (°C/min)",type:"number",value:10,min:10,max:20,step:1},
+            {label:"Temperature (°C)",name:"temperature",type:"number",value:100,step:5,validate:[{type:'numeric'}]},
+            {label:"Ramp (°C/min)",name:"ramp",type:"number",value:10,min:10,max:20,step:1},
             {label:"Hold time (min)",name:"hold_time",type:"number",value:0,min:0,max:5,step:1}
           ]},
           
