86515b07c5cbd813b7c0e13843f7ae0ce5de2680
Adam Smallcomb <atsmallcomb@gmail.com>
2022-10-17 11:21:11 -0400

    hide faas

--- docs/assets/js/devices.js
+++ docs/assets/js/devices.js
@@ -1,89 +1,85 @@
-
-devices ={
-
-  "HOME":{
-    device:"HOME",
-    components:['HOME'],
-    label:"Home",
-    disabled:true
+devices = {
+  HOME: {
+    device: "HOME",
+    components: ["HOME"],
+    label: "Home",
+    disabled: true,
   },
-    "GC":{
-      device:"GC",
-      components:['GC'],
-      label:"Gas Chromatograph",
-      image:"GC.png",
-      disabled:true
-    },
-    "CV":{
-      device:"CV",
-      components:['CV'],
-      label:"Cyclic Voltammeter",
-      image:"CV1.png"
-    },
-    "FTIS":{
-      device:"FTIS",
-      components:['FTIS'],
-      label:"Fourier Transform Infrared Spectrometer",
-      image:"spectrum 2.png"
-    },
-    "FTIR":{
-      device:"FTIR",
-      components:['FTIR'],
-      label:"Fourier Transform Infrared Spectrometer (FTIR)",
-      image:"ftir.png"
-    },
-    "F":{
-      device:"F",
-      components:['F'],
-      label:"Fluorimeter",
-      image:"ls-45.png"
-    },
-    "UV-Vis":{
-      device:"UV-Vis",
-      components:['UV-Vis'],
-      label:"UV-Vis Spectrophotometer",
-      image:"UVVis1.png"
-    },
-    "GC-MS":{
-      device:"GC-MS",
-      components:['GC-MS'],
-      label:"Gas Chromatograph - Mass Spectrometer",
-      image:"ag_gcms.png",
-      disabled:true
-    },
-    "HPLC":{
-      device:"HPLC",
-      components:['HPLC'],
-      label:"High Performance Liquid Chromatograph",
-      image:"HPLC1.png",
-      disabled:true
-    },
-    // "NMR":{
-    //   device:"NMR",
-    //   components:['NMR'],
-    //   label:"Nuclear Magnetic Resonance"
-    // },
-    "FAAS":{
-      device:"FAAS",
-      components:['FAAS'],
-      label:"Flame Atomic Absorption Spectrophotometer",
-      image:"HPLC1.png"
-    },
-    "DSC":{
-      device:"DSC",
-      components:['DSC'],
-      label:"Differential Scanning Calorimeter",
-      image:"DSC.png"
-    },
-    // "E":{
-    //   device:"E",
-    //   components:['E'],
-    //   label:"Electrophoresis"
-    // },
-    "Upload":{
-      components:['Upload'],
-      label:"Upload"
-    }
-  
-  }
-  
+  GC: {
+    device: "GC",
+    components: ["GC"],
+    label: "Gas Chromatograph",
+    image: "GC.png",
+    disabled: true,
+  },
+  CV: {
+    device: "CV",
+    components: ["CV"],
+    label: "Cyclic Voltammeter",
+    image: "CV1.png",
+  },
+  FTIS: {
+    device: "FTIS",
+    components: ["FTIS"],
+    label: "Fourier Transform Infrared Spectrometer",
+    image: "spectrum 2.png",
+  },
+  FTIR: {
+    device: "FTIR",
+    components: ["FTIR"],
+    label: "Fourier Transform Infrared Spectrometer (FTIR)",
+    image: "ftir.png",
+  },
+  F: {
+    device: "F",
+    components: ["F"],
+    label: "Fluorimeter",
+    image: "ls-45.png",
+  },
+  "UV-Vis": {
+    device: "UV-Vis",
+    components: ["UV-Vis"],
+    label: "UV-Vis Spectrophotometer",
+    image: "UVVis1.png",
+  },
+  "GC-MS": {
+    device: "GC-MS",
+    components: ["GC-MS"],
+    label: "Gas Chromatograph - Mass Spectrometer",
+    image: "ag_gcms.png",
+    disabled: true,
+  },
+  HPLC: {
+    device: "HPLC",
+    components: ["HPLC"],
+    label: "High Performance Liquid Chromatograph",
+    image: "HPLC1.png",
+    disabled: true,
+  },
+  // "NMR":{
+  //   device:"NMR",
+  //   components:['NMR'],
+  //   label:"Nuclear Magnetic Resonance"
+  // },
+  // "FAAS":{
+  //   device:"FAAS",
+  //   components:['FAAS'],
+  //   label:"Flame Atomic Absorption Spectrophotometer",
+  //   image:"HPLC1.png"
+  // },
+  DSC: {
+    device: "DSC",
+    components: ["DSC"],
+    label: "Differential Scanning Calorimeter",
+    image: "DSC.png",
+  },
+  // "E":{
+  //   device:"E",
+  //   components:['E'],
+  //   label:"Electrophoresis"
+  // },
+  Upload: {
+    components: ["Upload"],
+    label: "Upload",
+  },
+};
